64cdffecec4d3eb899c83a6caf064528c3f62b3d
HsiangHo <ho_cheung@qq.com>
2018-04-11 19:23:11 +0800

    add Exercise-01

---
+++ Exercise-01.c
@@ -0,0 +1,37 @@
+//
+//  Exercise-01.c
+//  Exercise
+//
+//  Created by Jovi on 4/11/18.
+//  Copyright © 2018 Jovi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("My name: Xiang He\n");
+    printf("中文名字: 何翔\n");
+    
+    printf("Born in the 1990s.\n");
+    printf("Love to play computer since 1998.\n");
+    printf("A developer from Chengdu, China.\n");
+    printf("Majored in Economics at SICAU.\n");
+    
+    printf("Favorite games:\n");
+    printf("Battlefiled series, GTA series, Red Alert 2, Super Mario Bros, CS, PUBG, LOL and so on.\n");
+    
+    printf("Favorite dramas:\n");
+    printf("Prison Break, Inside NO.9, Black Mirror, 東京ラブストーリー, リーガル・ハイ and so on.\n");
+    
+    printf("Favorite movies:\n");
+    printf("😄 So many!\n");
+    
+    printf("Learning C since 2012.\n");
+    printf("When I was at university, I wrote a love letter ✉️ in C and fell in love with a lovely girl.\n");
+    
+    printf("Nice to meet you!\n");
+    
+    return 0;
+}
