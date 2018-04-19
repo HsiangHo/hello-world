35fa193dbd866257bcffc36501c7299b3e4315ac
HsiangHo <ho_cheung@qq.com>
2018-04-20 00:48:42 +0800

    add Exercise-03

---
+++ Exercise-03.m
@@ -0,0 +1,26 @@
+//
+//  Exercise-03.m
+//  Exercise
+//
+//  Created by Jovi on 4/19/18.
+//  Copyright © 2018 Jovi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSLog(@"今天是2018年4月20日!");
+        NSLog(@"成都有一点电闪雷鸣和雨!");
+        NSLog(@"最近的音乐随机的都是日语歌，很温柔啊!");
+        NSLog(@"正在做5月底去北美计划，应该会非常充实!");
+        NSLog(@"想想自己一个人，难免深夜时也会感到孤独吧!");
+        NSLog(@"觉得缘分是一个好东西，所以前行的路上才不急不躁不紧不慢！");
+        NSLog(@"喜欢菠萝还是凤梨的问题，到现在也想明白了!");
+        NSLog(@"每一天，都要努力找到喜欢的人！");
+        NSLog(@"PHP是世界上最好的语言！");
+    }
+    return 0;
+}
